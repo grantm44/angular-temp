f93fe30e81fb27783686d4ba75ee6d9b0d376cc8
Grant McDonnal <grantmcdonnal@gmail.com>
2019-06-09 18:15:11 -0500

    Adds rental list component, rental detail component, rental service and rental model

---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,14 @@
+export class Rental {
+    id: number;
+    title: string;
+    city: string;
+    street: string;
+    image: string;
+    category: string;
+    bedrooms: number;
+    description: string;
+    dailyRate: number;
+    shared: boolean;
+    createdAt: string;
+
+}
